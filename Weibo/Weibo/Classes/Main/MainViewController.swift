0694b268b03c9ea75cf75f6579b06cb194c161c4
524429264 <524429264@qq.com>
2016-04-15 00:03:58 +0800

    动态加载命名空间

--- Weibo/Weibo/Classes/Main/MainViewController.swift
+++ Weibo/Weibo/Classes/Main/MainViewController.swift
@@ -15,21 +15,33 @@ class MainViewController: UITabBarController {
         
         tabBar.tintColor = UIColor.orangeColor()
         // Do any additional setup after loading the view.
-        addChildViewController(HomeViewController(), title: "首页", imageNamed: "tabbar_home")
-        addChildViewController(MessageViewController(), title: "消息", imageNamed: "tabbar_message_center")
+        addChildViewController("HomeViewController", title: "首页", imageNamed: "tabbar_home")
+        addChildViewController("MessageViewController", title: "消息", imageNamed: "tabbar_message_center")
 
-        addChildViewController(DiscoverViewController(), title: "广场", imageNamed: "tabbar_discover")
-        addChildViewController(ProfileViewController(), title: "我", imageNamed: "tabbar_profile")
+        addChildViewController("DiscoverViewController", title: "广场", imageNamed: "tabbar_discover")
+        addChildViewController("ProfileViewController", title: "我", imageNamed: "tabbar_profile")
 
     }
 
-    private func addChildViewController(childController: UIViewController, title:String, imageNamed:String) {
-        childController.tabBarItem.image = UIImage(named: imageNamed)
-        childController.tabBarItem.selectedImage = UIImage(named: imageNamed + "_highlighted")
-        childController.title = title
+    private func addChildViewController(childControllerName: String, title:String, imageNamed:String) {
+
+        let ns = NSBundle.mainBundle().infoDictionary!["CFBundleName"] as! String
+        let cls:AnyClass? = NSClassFromString(ns + "." + childControllerName)
+        
+        let vcCls = cls as! UIViewController.Type
+        let vc = vcCls.init()
+        
+        
+        
+        
+        
+        
+        vc.tabBarItem.image = UIImage(named: imageNamed)
+        vc.tabBarItem.selectedImage = UIImage(named: imageNamed + "_highlighted")
+        vc.title = title
         
         let nav = UINavigationController()
-        nav.addChildViewController(childController)
+        nav.addChildViewController(vc)
         
         addChildViewController(nav)
         
